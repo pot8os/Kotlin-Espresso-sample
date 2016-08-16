d75722f452b23340bfdbc3dc202d9bba78ab0c87
pot8os <mygithub@pot8os.net>
2016-08-16 21:34:33 +0900

    Updated unit test

--- app/src/test/kotlin/net/pot8os/kotlintestsample/CalculatorTest.kt
+++ app/src/test/kotlin/net/pot8os/kotlintestsample/CalculatorTest.kt
@@ -4,22 +4,22 @@ import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.not
 import org.junit.Assert.assertThat
 import org.junit.Test
+import java.math.BigDecimal
 
 /**
  * @author So Nakamura, 2015/12/19
  */
 class CalculatorTest {
 
-    private val calclator = Calculator()
-
     @Test
-    fun normalTest() {
-        assertThat(calclator.calc("100", "200"), `is`("300"))
-        assertThat(calclator.calc("1", "1"), `is`(not("3")))
-    }
-
-    @Test(expected = NumberFormatException::class)
-    fun exceptionTest() {
-        calclator.calc("", "")
+    fun figureTest() {
+        assertThat(MainActivity.Figure.ADD.calc(BigDecimal(100), BigDecimal(200)),
+                `is`(BigDecimal(300)))
+        assertThat(MainActivity.Figure.SUB.calc(BigDecimal(10), BigDecimal(4)),
+                `is`(not(BigDecimal(5))))
+        assertThat(MainActivity.Figure.MULTI.calc(BigDecimal(5), BigDecimal(5)),
+                `is`(BigDecimal(25)))
+        assertThat(MainActivity.Figure.DIV.calc(BigDecimal(10), BigDecimal(3)),
+                `is`(not(BigDecimal(3.33333333))))
     }
 }
