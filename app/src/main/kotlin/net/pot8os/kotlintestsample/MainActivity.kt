b6d276b4f6ef86223650f2aa2a06cd5cd647092f
pot8os <mygithub@pot8os.net>
2016-08-16 21:34:10 +0900

    Updated main activity

--- app/src/main/kotlin/net/pot8os/kotlintestsample/MainActivity.kt
+++ app/src/main/kotlin/net/pot8os/kotlintestsample/MainActivity.kt
@@ -1,38 +1,75 @@
 package net.pot8os.kotlintestsample
 
-import android.app.AlertDialog
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import com.jakewharton.rxbinding.view.RxView
+import net.pot8os.kotlintestsample.databinding.ActivityMainBinding
+import java.math.BigDecimal
+import java.text.DecimalFormat
 
 /**
  * @author So Nakamura, 2015/12/19
  */
 class MainActivity : AppCompatActivity() {
 
+    enum class Figure {
+        ADD, SUB, MULTI, DIV, NONE;
+
+        fun calc(arg1: BigDecimal, arg2: BigDecimal): BigDecimal {
+            when (this) {
+                Figure.ADD -> return arg1.plus(arg2)
+                Figure.SUB -> return arg1.minus(arg2)
+                Figure.MULTI -> return arg1.multiply(arg2)
+                Figure.DIV -> return arg1.divide(arg2, 8, BigDecimal.ROUND_HALF_UP)
+                Figure.NONE -> return arg2
+            }
+        }
+    }
+
+    private val formatter = DecimalFormat("#.#").apply {
+        minimumFractionDigits = 0
+        maximumFractionDigits = 8
+    }
+
+    private var field = BigDecimal.ZERO
+    private var stack = BigDecimal.ZERO
+    private var currentFigure = Figure.NONE
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var calcButton = findViewById(R.id.calcButton) as Button
-        calcButton.setOnClickListener {
-            val arg1 = findViewById(R.id.arg1) as EditText
-            val arg2 = findViewById(R.id.arg2) as EditText
-            val calculator = Calculator()
-            try {
-                val answer = calculator.calc(arg1.text.toString(), arg2.text.toString())
-                val answerView = findViewById(R.id.answer) as TextView
-                answerView.text = answer
-            } catch (e: Exception) {
-                Log.wtf("Exception.", e)
-                AlertDialog.Builder(this)
-                        .setTitle("Error")
-                        .setMessage("Invalid input. Set numeric string.")
-                        .setPositiveButton("OK", null)
-                        .show()
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        for (button in arrayOf(binding.button0,
+                binding.button1, binding.button2, binding.button3,
+                binding.button4, binding.button5, binding.button6,
+                binding.button7, binding.button8, binding.button9)) {
+            RxView.clicks(button).subscribe {
+                field = field.multiply(BigDecimal(10)).plus(BigDecimal(button.tag.toString().toInt()))
+                binding.field.setText(formatter.format(field))
             }
+        }
+
+        for (button in arrayOf(binding.buttonAllClear,
+                binding.buttonAdd, binding.buttonSub,
+                binding.buttonMulti, binding.buttonDivide)) {
+            RxView.clicks(button).subscribe {
+                currentFigure = Figure.valueOf(button.tag.toString())
+                stack = if (currentFigure != Figure.NONE) {
+                    field
+                } else {
+                    BigDecimal.ZERO
+                }
+                field = BigDecimal.ZERO
+                if (stack == BigDecimal.ZERO) {
+                    binding.field.setText(formatter.format(field))
+                }
+            }
+        }
+
+        RxView.clicks(binding.buttonCalc).subscribe {
+            field = currentFigure.calc(stack, field)
+            binding.field.setText(formatter.format(field))
         }
     }
 }
