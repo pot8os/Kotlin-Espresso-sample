bbaf457a4887ee63b6e08eab6a55a235cfbe8eb6
So Nakamura <web_github@pot8os.net>
2019-03-16 12:25:05 +0900

    Use new test runner class

--- app/src/androidTest/kotlin/net/pot8os/kotlintestsample/ApplicationTest.kt
+++ app/src/androidTest/kotlin/net/pot8os/kotlintestsample/ApplicationTest.kt
@@ -5,8 +5,8 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,9 +18,8 @@ import java.text.DecimalFormat
 @RunWith(AndroidJUnit4::class)
 class ApplicationTest {
 
-  @Rule
-  @JvmField
-  val activity = ActivityTestRule<MainActivity>(MainActivity::class.java)
+  @get:Rule
+  val activity = ActivityTestRule(MainActivity::class.java)
 
   @Test
   fun testAdd() {
